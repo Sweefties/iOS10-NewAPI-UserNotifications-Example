702b864e72345f60283e4f1d183b5b633843ceb0
Wlad Dicario <wladdicario@macbook-pro-de-wlad.home>
2016-09-22 17:53:06 +0200

    Initial Commit

---
+++ iOS10-NewAPI-UserNotifications-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS10-NewAPI-UserNotifications-Example
+//
+//  Created by Wlad Dicario on 22/09/2016.
+//  Copyright © 2016 Sweefties. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
